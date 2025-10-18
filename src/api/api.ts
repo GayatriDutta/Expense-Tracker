56e58487bb3c4dd8fa4d6f0dd95ac4beb0f7ccb5
GayatriDutta <69198031+GayatriDutta@users.noreply.github.com>
2025-10-18 21:47:03 +0530

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Backend URL
+  baseURL: 'VITE_API_URL=https://your-render-backend.onrender.com', 
 });
 
 api.interceptors.request.use(config => {
